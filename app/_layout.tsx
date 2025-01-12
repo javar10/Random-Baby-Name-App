19a105e2c12eb6432da4adbc0359ca4fc1d9a4b0
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2025-01-12 15:37:16 -0500

    Header Setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: '#31F99A' },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: 'Name My Baby!' }} />
+    </Stack>
+  )
+    ;
 }
