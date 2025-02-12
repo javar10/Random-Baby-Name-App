ec3d273cf7bb075b8737f017cbcefeae308fa425
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2025-02-12 16:21:51 -0500

    font modal styling

--- app/components/options/ChooseFont.tsx
+++ app/components/options/ChooseFont.tsx
@@ -1,9 +1,10 @@
-import { Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import Modal from "react-native-modal";
-import styles from "./OptionsMenuStyles";
+import { default as optionsStyles } from "./OptionsMenuStyles";
+import { default as modalStyles } from '../name/ModalStyles'
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faFont } from "@fortawesome/free-solid-svg-icons";
+import { faFont, faXmark } from "@fortawesome/free-solid-svg-icons";
 import DropDownPicker from "react-native-dropdown-picker";
 import fontList from "@/app/constants/fontList";
 
@@ -27,43 +28,63 @@ const ChooseFont: React.FC<Props> = ({ setNameFont }) => {
                 isVisible={modalOpen}
                 onBackdropPress={() => setModalOpen(false)}
             >
-                <DropDownPicker
-                    open={fontsOpen}
-                    value={selectedFont}
-                    items={fontArray}
-                    setOpen={setFontsOpen}
-                    setValue={setSelectedFont}
-                    textStyle={{ fontFamily: selectedFont }}
+                <View style={modalStyles.modalOverlay}>
+                    <View style={modalStyles.container}>
 
-                    renderListItem={(props) => {
-                        const { item, onPress } = props;
-                        return (
-                            <TouchableOpacity
-                                onPress={() => {
-                                    setSelectedFont(String(item.value))
-                                    setModalOpen(false)
-                                    setFontsOpen(false)
-                                    setNameFont(String(item.value))
-                                    console.log(selectedFont)
+                        <View style={modalStyles.header}>
+                            <Text style={modalStyles.headerText}>Select a font</Text>
+                        </View>
+                        <View style={modalStyles.content}>
+                            <DropDownPicker
+                                open={fontsOpen}
+                                value={selectedFont}
+                                items={fontArray}
+                                setOpen={setFontsOpen}
+                                setValue={setSelectedFont}
+                                textStyle={[modalStyles.contentText, { fontFamily: selectedFont }]}
+                                style={{marginVertical: 10}}
+                                renderListItem={(props) => {
+                                    const { item, onPress } = props;
+                                    return (
+                                        <TouchableOpacity
+                                            onPress={() => {
+                                                setSelectedFont(String(item.value))
+                                                setModalOpen(false)
+                                                setFontsOpen(false)
+                                                setNameFont(String(item.value))
+                                                console.log(selectedFont)
+                                            }}
+                                            style={{
+                                                padding: 10,
+                                                borderBottomWidth: 1,
+                                                borderBottomColor: "#ddd",
+                                            }}
+                                        >
+                                            <Text 
+                                            // style={{ fontFamily: item.value, fontSize: 18, color: "black" }}
+                                            style={[modalStyles.contentText, { fontFamily: item.value }]}
+                                            >
+                                                {item.label}
+                                            </Text>
+                                        </TouchableOpacity>
+                                    );
                                 }}
-                                style={{
-                                    padding: 10,
-                                    borderBottomWidth: 1,
-                                    borderBottomColor: "#ddd",
-                                }}
-                            >
-                                <Text style={{ fontFamily: item.value, fontSize: 18, color: "black" }}>
-                                    {item.label}
-                                </Text>
+
+                            />
+                        </View>
+                        <View style={modalStyles.footer}>
+                            <TouchableOpacity style={modalStyles.footerButton} onPress={() => setModalOpen(false)}>
+                                <FontAwesomeIcon style={modalStyles.footerIcon} icon={faXmark} />
                             </TouchableOpacity>
-                        );
-                    }}
 
-                />
-            </Modal>
+                        </View>
+                    </View>
+                </View>
+            </Modal >
 
-            <TouchableOpacity style={styles.optionsMenuButton} onPress={() => setModalOpen(true)} >
-                <FontAwesomeIcon style={styles.optionsMenuIcon} icon={faFont} />
+
+            <TouchableOpacity style={optionsStyles.optionsMenuButton} onPress={() => setModalOpen(true)} >
+                <FontAwesomeIcon style={optionsStyles.optionsMenuIcon} icon={faFont} />
             </TouchableOpacity>
         </>
 
